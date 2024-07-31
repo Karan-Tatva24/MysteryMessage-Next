9ac6fa5baf44b1f5af0e88d66d01678b28d66e66
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-07-31 15:13:48 +0530

    Add send message page and integrate

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -108,7 +108,7 @@ const Dashboard = () => {
     }
   };
 
-  const { username } = session?.user as User;
+  const username = session?.user?.username as User["username"];
 
   const baseUrl = `${window.location.protocol}//${window.location.host}`;
   const profileUrl = `${baseUrl}/u/${username}`;
